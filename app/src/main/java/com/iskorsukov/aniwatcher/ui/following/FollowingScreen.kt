e096804958e8bcdaf3b96391e6e368507dc058c1
Korsukov Ilya <iskorsukov@gmail.com>
2022-11-09 16:48:39 +0300

    Add empty content indicator for following screen

--- app/src/main/java/com/iskorsukov/aniwatcher/ui/following/FollowingScreen.kt
+++ app/src/main/java/com/iskorsukov/aniwatcher/ui/following/FollowingScreen.kt
@@ -1,31 +1,75 @@
 package com.iskorsukov.aniwatcher.ui.following
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.iskorsukov.aniwatcher.R
 import com.iskorsukov.aniwatcher.domain.mapper.MediaItemMapper
 import com.iskorsukov.aniwatcher.test.ModelTestDataCreator
 import com.iskorsukov.aniwatcher.test.isFollowing
 import com.iskorsukov.aniwatcher.ui.media.MediaItemCardExtended
+import com.iskorsukov.aniwatcher.ui.theme.CardTextColorLight
 import kotlinx.coroutines.flow.Flow
 
 @OptIn(ExperimentalLifecycleComposeApi::class)
 @Composable
 fun FollowingScreen(viewModel: FollowingViewModel = viewModel(), timeInMinutesFlow: Flow<Long>) {
-    val followingMediaFlow by viewModel.followingMediaFlow
+    val followingMediaMap by viewModel.followingMediaFlow
         .collectAsStateWithLifecycle(initialValue = emptyMap())
 
     val timeInMinutes by timeInMinutesFlow
         .collectAsStateWithLifecycle(initialValue = 0)
 
+    if (followingMediaMap.isEmpty()) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            Column(
+                modifier = Modifier
+                    .align(Alignment.Center),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Icon(
+                    painter = painterResource(
+                        id = R.drawable.ic_baseline_add_circle_outline_24_gray
+                    ),
+                    contentDescription = null,
+                    tint = CardTextColorLight,
+                    modifier = Modifier.size(36.dp)
+                )
+                Text(
+                    text = stringResource(
+                        id = R.string.following_data_empty_label
+                    ),
+                    color = CardTextColorLight,
+                    fontSize = 16.sp
+                )
+                Text(
+                    text = stringResource(
+                        id = R.string.following_data_empty_sub_label
+                    ),
+                    color = CardTextColorLight,
+                    fontSize = 12.sp
+                )
+            }
+        }
+    }
     LazyColumn(modifier = Modifier.fillMaxSize()) {
-        followingMediaFlow.entries.forEach {
+        followingMediaMap.entries.forEach {
             item {
                 MediaItemCardExtended(
                     mediaItem = it.key,
@@ -34,6 +78,43 @@ fun FollowingScreen(viewModel: FollowingViewModel = viewModel(), timeInMinutesFl
                     onFollowClicked = viewModel::onFollowClicked
                 )
             }
+        }
+    }
+}
+
+@Composable
+@Preview
+fun FollowingScreenEmptyPlaceholderPreview() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier
+                .align(Alignment.Center), horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(
+                painter = painterResource(
+                    id = R.drawable.ic_baseline_add_circle_outline_24_gray
+                ),
+                contentDescription = null,
+                tint = CardTextColorLight,
+                modifier = Modifier.size(36.dp)
+            )
+            Text(
+                text = stringResource(
+                    id = R.string.following_data_empty_label
+                ),
+                color = CardTextColorLight,
+                fontSize = 16.sp
+            )
+            Text(
+                text = stringResource(
+                    id = R.string.following_data_empty_sub_label
+                ),
+                color = CardTextColorLight,
+                fontSize = 12.sp
+            )
         }
     }
 }
