d3fd099272c9b6395e682fab2f2eb78419a52ac0
Korsukov Ilya <iskorsukov@gmail.com>
2022-11-26 09:20:38 +0300

    Add icon

--- app/src/main/java/com/iskorsukov/aniwatcher/domain/notification/work/util/NotificationBuilderHelper.kt
+++ app/src/main/java/com/iskorsukov/aniwatcher/domain/notification/work/util/NotificationBuilderHelper.kt
@@ -14,7 +14,7 @@ object NotificationBuilderHelper {
             context,
             NotificationsWorker.CHANNEL_ID
         )
-            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setSmallIcon(R.drawable.aniwatcher_icon_fg_white)
             .setContentTitle(airingScheduleItem.mediaItem.title.baseText())
             .setContentText(
                 String.format(
