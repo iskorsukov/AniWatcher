2548925056c93ccd7d3fd6679fb2b97bc7b9f09d
Korsukov Ilya <iskorsukov@gmail.com>
2022-12-20 15:15:59 +0300

    Filter unavailable media formats

--- app/src/main/java/com/iskorsukov/aniwatcher/data/mapper/QueryDataToEntityMapper.kt
+++ app/src/main/java/com/iskorsukov/aniwatcher/data/mapper/QueryDataToEntityMapper.kt
@@ -4,6 +4,7 @@ import com.iskorsukov.aniwatcher.RangeAiringDataQuery
 import com.iskorsukov.aniwatcher.SeasonAiringDataQuery
 import com.iskorsukov.aniwatcher.data.entity.AiringScheduleEntity
 import com.iskorsukov.aniwatcher.data.entity.MediaItemEntity
+import com.iskorsukov.aniwatcher.domain.model.MediaItem
 import javax.inject.Inject
 
 class QueryDataToEntityMapper @Inject constructor() {
@@ -12,7 +13,9 @@ class QueryDataToEntityMapper @Inject constructor() {
         if (data.Page?.media == null || data.Page.media.any { it?.airingSchedule?.airingScheduleNode == null }) {
             throw IllegalArgumentException("Unexpected null value in query data")
         }
-        return data.Page.media.filterNotNull().associate {
+        return data.Page.media
+            .filterNotNull()
+            .associate {
             MediaItemEntity.fromData(it) to
                     it.airingSchedule!!.airingScheduleNode!!.filterNotNull().map {
                         AiringScheduleEntity.fromData(it)
@@ -27,6 +30,7 @@ class QueryDataToEntityMapper @Inject constructor() {
         }
         return data.Page.airingSchedules
             .filterNotNull()
+            .filter { it.media?.format == null || MediaItem.LocalFormat.values().map { it.name }.contains(it.media.format.name) }
             .filter { it.media?.isAdult != null && it.media.isAdult == false }
             .associate { outerSchedule ->
             val media = outerSchedule.media!!
